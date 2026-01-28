3fa9b14957979738fada6902d843e5111a1fdaa6
bryan <bryan.stewart@powerwyze.com>
2026-01-27 19:08:52 -0500

    Fix production assets and Tailwind build

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./index.tsx",
+    "./App.tsx",
+    "./components/**/*.{ts,tsx}",
+    "./services/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
